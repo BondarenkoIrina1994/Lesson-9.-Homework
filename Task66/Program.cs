252e64113813a0b4b7165d9486e0382669dace56
«Irina <bondarenko-irinka@mail.ru>
2022-06-29 21:50:04 +0300

    task66,68

---
+++ Task66/Program.cs
@@ -0,0 +1,40 @@
+﻿
+// Задача 66: 
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int CheckingForCorrectnes(string str)
+{
+    while (true)
+    {
+        Console.WriteLine(str);
+        string num = Console.ReadLine();
+        if (int.TryParse(num, out int number) == false)
+            Console.WriteLine("Некорректное значение. Для корректной работы программы введите натуральное число!!!");
+        else
+        {
+            if (number < 0)
+                Console.WriteLine("Необходимо ввести натуральное число!!!");
+            else
+                return number;
+        }
+    }
+}
+int SumElements(int number1, int number2)
+{
+    if (number1 == number2) return number1;
+    else return number2 + SumElements(number1, number2 - 1);
+}
+int m = CheckingForCorrectnes("Введите натуральное число M:");
+int n = CheckingForCorrectnes("Введите натуральное число N:");
+int result;
+if (n > m)
+{
+    result = SumElements(m, n);
+    Console.WriteLine($"Cумма натуральных элементов в промежутке от {m} до {n} равна: {result}");
+}
+else
+{
+    result = SumElements(n, m);
+    Console.WriteLine($"Cумма натуральных элементов в промежутке от {n} до {m} равна: {result}");
+}
---
+++ Task68/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+int CheckingForCorrectnes(string str)
+{
+    while (true)
+    {
+        Console.WriteLine(str);
+        string num = Console.ReadLine();
+        if (int.TryParse(num, out int number) == false)
+            Console.WriteLine("Некорректное значение. Для корректной работы программы введите целое число!!!");
+        else
+        {
+            if (number < 0)
+                Console.WriteLine("Необходимо ввести положительное число!!!");
+            else
+                return number;
+        }
+    }
+}
+int FunctionAckerman(int m, int n) 
+{ 
+    if (m==0) return n+1; 
+    else if (m>0 && n==0) return FunctionAckerman(m-1, 1); 
+    else return FunctionAckerman(m-1, FunctionAckerman(m, n-1)); 
+}
+int m = CheckingForCorrectnes("Задайте значение M:");
+int n = CheckingForCorrectnes("Задайте значение N:");
+int result=FunctionAckerman(m,n);
+Console.WriteLine($"Значение функции Аккермана при исходных данных M={m} и N={n} равно: {result}");
